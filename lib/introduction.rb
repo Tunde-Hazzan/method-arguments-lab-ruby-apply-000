5f12edaf03a38c8b45985d7fd561b77feef601d4
Tunde Hazzan <t.hazzan@ctrlmetrics.com>
2018-07-07 13:33:35 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name,language)
-  puts ""
-
-Define a method, `#introduction_with_language` that takes in two arguments, `name` and `language` and outputs the phrase: `"Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
